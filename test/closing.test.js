4b78de1ee0f64e696d230bf48e762e8763932ff5
Paolo Insogna <paolo@cowtech.it>
2025-02-18 17:08:48 +0100

    feat: Add restart.
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- test/closing.test.js
+++ test/closing.test.js
@@ -2,7 +2,7 @@
 
 const autocannon = require('autocannon')
 const { test } = require('node:test')
-const { deepStrictEqual, strictEqual, rejects } = require('node:assert')
+const { deepStrictEqual, strictEqual, rejects, throws } = require('node:assert')
 const { join } = require('path')
 const { Worker } = require('worker_threads')
 const { Agent, request } = require('undici')
@@ -88,9 +88,7 @@ test('graceful close from the different threads', async t => {
     deepStrictEqual(await body.json(), { delayed: true })
   }
 
-  interceptor.close()
-
-  await waitMessage(composer, message => message.type === MESSAGE_CLOSE)
+  await interceptor.close()
 
   await rejects(() => request('http://composer.local/s1/example?foo=bar', { dispatcher: agent }), {
     message: `No target found for composer.local in thread ${threadId}.`
@@ -128,16 +126,55 @@ test('no requests are lost if two threads are swapped in the same iteration of t
   })
 
   // Every 500ms, swap the worker
-  setInterval(() => {
+  const interval = setInterval(() => {
     const newWorker = new Worker(join(__dirname, 'fixtures', 'graceful-close.js'))
     t.after(() => newWorker.terminate())
     workers.unshift(newWorker)
 
     interceptor.route('myserver', workers[0])
     workers[1].postMessage('close')
-  }, 500).unref()
+  }, 500)
 
   const results = await resultsPromise
+  clearInterval(interval)
   deepStrictEqual(results.errors, 0)
   deepStrictEqual(results.non2xx, 0)
+})
+
+test('routes are rejected if the dispatcher is closed', async t => {
+  const worker1 = new Worker(join(__dirname, 'fixtures', 'worker1.js'))
+  t.after(() => worker1.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local'
+  })
+  interceptor.route('myserver', worker1)
+
+  const agent = new Agent().compose(interceptor)
+
+  {
+    const { statusCode, body } = await request('http://myserver.local', { dispatcher: agent })
+    strictEqual(statusCode, 200)
+    deepStrictEqual(await body.json(), { hello: 'world' })
+  }
+
+  await interceptor.close()
+
+  await rejects(() => request('http://myserver.local', { dispatcher: agent }), {
+    message: 'No target found for myserver.local in thread 0.'
+  })
+
+  await throws(() => interceptor.route('myserver', worker1), { message: 'The dispatcher has been closed.' })
+
+  interceptor.restart()
+
+  const worker2 = new Worker(join(__dirname, 'fixtures', 'worker1.js'))
+  t.after(() => worker2.terminate())
+  interceptor.route('myserver', worker2)
+
+  {
+    const { statusCode, body } = await request('http://myserver.local', { dispatcher: agent })
+    strictEqual(statusCode, 200)
+    deepStrictEqual(await body.json(), { hello: 'world' })
+  }
 })
