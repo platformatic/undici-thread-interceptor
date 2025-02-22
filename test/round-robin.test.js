52b8ffd1dbbb6ad1901111de4256a0f465edd252
Paolo Insogna <paolo@cowtech.it>
2025-02-22 19:39:17 +0100

    feat: Added unroute method. (#72)

--- test/round-robin.test.js
+++ test/round-robin.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { test } = require('node:test')
-const { deepStrictEqual, rejects, strictEqual } = require('node:assert')
+const { deepStrictEqual, rejects, strictEqual, throws } = require('node:assert')
 const { join } = require('path')
 const { Worker } = require('worker_threads')
 const { createThreadInterceptor } = require('../')
@@ -215,4 +215,55 @@ test('503 status code retries it', async t => {
     strictEqual(statusCode, 200)
     deepStrictEqual(await body.json(), { threadId: worker2.threadId })
   }
+})
+
+test('round-robin multiple add/remove operations', async t => {
+  const worker1 = new Worker(join(__dirname, 'fixtures', 'worker1.js'), {
+    workerData: { message: 'mesh' }
+  })
+  t.after(() => worker1.terminate())
+  const worker2 = new Worker(join(__dirname, 'fixtures', 'worker1.js'))
+  t.after(() => worker2.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local'
+  })
+  interceptor.route('myserver', worker1)
+  interceptor.addRoute('myserver', worker2)
+
+  const agent = new Agent().compose(interceptor)
+  async function whoami (expectedWorker) {
+    const { body } = await request('http://myserver.local/whoami', { dispatcher: agent })
+
+    deepStrictEqual(await body.json(), { threadId: expectedWorker.threadId })
+  }
+
+  await whoami(worker1)
+  await whoami(worker2)
+
+  interceptor.unroute('myserver', worker1)
+  // This should be a no-op since the worker no longer in the round-robin
+  interceptor.removeRoute('myserver', worker1)
+
+  await whoami(worker2)
+  await whoami(worker2)
+
+  interceptor.route('myserver', worker1)
+  // This should be a no-op since the worker is already in the round-robin
+  interceptor.addRoute('myserver', worker2)
+
+  await whoami(worker2)
+  await whoami(worker1)
+
+  await interceptor.unroute('myserver', worker1)
+  await interceptor.removeRoute('myserver', worker2)
+
+  await rejects(() => request('http://myserver.local/whoami', { dispatcher: agent }), {
+    message: 'No target found for myserver.local in thread 0.'
+  })
+
+  await interceptor.close()
+
+  await throws(() => interceptor.route('myserver', worker1), { message: 'The dispatcher has been closed.' })
+  await throws(() => interceptor.unroute('myserver', worker1), { message: 'The dispatcher has been closed.' })
 })
